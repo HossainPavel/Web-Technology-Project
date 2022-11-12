c02eb3f3c604acce5f45b778d09435889bddbbae
HossainPavel <89180472+HossainPavel@users.noreply.github.com>
2022-11-12 13:07:00 +0600

    Add files via upload
    Web Development: Created ”Property Management Systems” using HTML, CSS, PHP,JavaScript, AJAX, J-QUERY and MySQL(PHP_ADMIN).

---
+++ Web_Final/Admin/MainPage.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/Mainpage.css">
+<title>Main Form </title>
+
+</head>
+<body style="background-image: url('back10.jpg');">
+<div class= "header sticky">
+    <h1 align="center" style="color: navy;">Property Management System</h1> 
+</div>
+<form action="" method="post">
+
+<table align="center" style="width:60%;">
+<tr><td <h5 align='center'><a href="view\Login.php"><p style="background-color:lightyellow;">Admin</p> </h5></td></tr>
+</table>
+
+
+
+</form>
+
+
+
+
+</body>
+</html>
